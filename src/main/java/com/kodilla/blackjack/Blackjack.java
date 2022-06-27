b2f08ba0e30983b4cad128be9f754e9c859564b6
jacolec <jacwydro@gmail.com>
2022-06-27 20:03:21 +0200

    blackjack

---
+++ src/main/java/com/kodilla/blackjack/Blackjack.java
@@ -0,0 +1,35 @@
+package com.kodilla.blackjack;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Blackjack extends Application {
+
+    private Image imageback = new Image("file:s/home/jacolec/Obrazy/java_backgrounds/blackjack1.jpg");
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+
+        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
+
+        primaryStage.setTitle("BlackJack");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
